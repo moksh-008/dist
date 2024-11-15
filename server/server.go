59fd475c9b62a1d2b0d0fd69d44feadfbd806d51
mokshpatel <oc22442@bristol.ac.uk>
2024-11-15 17:46:44 +0000

    Distributed step 4 for aws

--- server/server.go
+++ server/server.go
@@ -65,6 +65,10 @@ func (s *GameOfLifeOperations) GOL(req stubs.Request, res *stubs.Response) (err 
 		GolTurn = t + 1 // Update the global turn count
 		mu.Unlock()
 
+		// Check for pause condition
+		for Pause == "Pause" {
+			time.Sleep(1 * time.Second)
+		}
 	}
 
 	// Populate the response with the final world state and alive cells after final state
@@ -77,6 +81,11 @@ func (s *GameOfLifeOperations) GOL(req stubs.Request, res *stubs.Response) (err 
 
 // Alive provides the count of currently alive cells and the current turn
 func (s *GameOfLifeOperations) Alive(req stubs.AliveRequest, res *stubs.AliveResponse) (err error) {
+
+	// Wait if the game is paused
+	for Pause == "Pause" {
+		time.Sleep(1 * time.Second)
+	}
 
 	// Calculate the alive cells based on the current world state
 	mu.Lock()
@@ -198,5 +207,4 @@ func main() {
 	defer listener.Close()
 	fmt.Println("Server started on port", *pAddr)
 	rpc.Accept(listener)
-
 }
